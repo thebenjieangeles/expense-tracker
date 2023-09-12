95bbdad2526b0337e56d051a05d3d5ca4c6fb9ec
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-09-12 22:00:47 +0800

    first commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDH7BjLczs_2ABAdJUiR0-wtKaZIIxadac",
+  authDomain: "expense-tracker-192bb.firebaseapp.com",
+  projectId: "expense-tracker-192bb",
+  storageBucket: "expense-tracker-192bb.appspot.com",
+  messagingSenderId: "512460668820",
+  appId: "1:512460668820:web:f235b8c29b1b415c469981",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
