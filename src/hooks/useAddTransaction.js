e0620380f227d96aee1d73e361d84241e668d132
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-09-14 20:57:43 +0800

    added hooks

---
+++ src/hooks/useAddTransaction.js
@@ -0,0 +1,23 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
+
+export const useAddTransaction = () => {
+  const transactionCollectionRef = collection(db, "transactions");
+  const { userID } = useGetUserInfo();
+
+  const addTransaction = async ({
+    description,
+    transactionAmount,
+    transactionType,
+  }) => {
+    await addDoc(transactionCollectionRef, {
+      userID,
+      description,
+      transactionAmount,
+      transactionType,
+      createAt: serverTimestamp(),
+    });
+  };
+  return { addTransaction };
+};
---
+++ src/pages/expense-tracker/index.jsx
@@ -0,0 +1,51 @@
+import { useAddTransaction } from "../../hooks/useAddTransaction";
+
+export const ExpenseTracker = () => {
+  const { addTransaction } = useAddTransaction();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    addTransaction({
+      description: "Haircut",
+      transactionAmount: 22,
+      transactionType: "expense",
+    });
+  };
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1>Expense Tracker</h1>
+          <div className="balance">
+            <h3>Your Balance</h3>
+            <h3>$0.00</h3>
+          </div>
+          <div className="summary">
+            <div className="income">
+              <h4>Income</h4>
+              <p>$0.00</p>
+            </div>
+            <div className="expenses">
+              <h4>Expenses</h4>
+              <p>$0.00</p>
+            </div>
+          </div>
+          <form className="add-transaction" onSubmit={onSubmit}>
+            <input type="text" placeholder="Description" required />
+            <input type="number" placeholder="Amount" required />
+            <input type="radio" id="expense" value="expense" />
+            <label htmlFor="expense">Expense</label>
+            <input type="radio" id="income" value="income" />
+            <label htmlFor="income">Income</label>
+
+            <button type="submit">Add Transaction</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="transactions">
+        <h3> Transactions</h3>
+      </div>
+    </>
+  );
+};
